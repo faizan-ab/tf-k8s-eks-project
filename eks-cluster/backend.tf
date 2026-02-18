575d97732d8500f0f93072a08c60332f2f3169dc
Ubuntu <ubuntu@ip-172-31-46-24.ap-south-1.compute.internal>
2026-02-18 14:35:40 +0000

    Fixed S3 backend bucket name in eks-cluster

--- eks-cluster/backend.tf
+++ eks-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-terraform-kubernetes-nginx"
+    bucket = "jenkins-terraform-kubernetes-nginx1"
     region = "ap-south-1"
     key    = "eks/terraform.tfstate"
   }
